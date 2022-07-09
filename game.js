3c32fb6325a8466d7d10988bd35dfd54b423231b
callumgordon90 <callumjosephiaingordon@gmail.com>
2022-07-09 21:59:23 +0200

    function 'checkDeath' and function draw in game.js file

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } from './snake.js'
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection, update, draw } from './snake.js'
 import { update as updateFood, draw as drawFood } from './food.js'
 import { outsideGrid } from './grid.js'
 
@@ -19,9 +19,26 @@ function main(currentTime) {
     const secondsSinceLastRender = ( currentTime - lastRenderTime) / 1000
     if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
 
+    lastRenderTime = currentTime
 
+    update()
+    draw()
+}
 
+window.requestAnimationFrame(main)
 
+function update() {
+    updateSnake()
+    updateFood()
+    checkDeath()
+}
 
+function draw() {
+    gameBoard.innerHTML = ''
+    drawSnake(gameBoard)
+    drawFood(gameBoard)
+}
 
+function checkDeath() {
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
 }
