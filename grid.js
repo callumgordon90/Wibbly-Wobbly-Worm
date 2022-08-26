31cf9531bd5795c037614c4e0be0fd57d102341e
callumgordon90 <callumjosephiaingordon@gmail.com>
2022-08-26 20:12:13 +0200

    corrected a syntax error in line 6 of grid.js (accidently used a comma instead of a point)

--- grid.js
+++ grid.js
@@ -3,7 +3,7 @@ const GRID_SIZE = 21
 
 export function randomGridPosition() {
     return {
-        x: Math.floor(Math,random() * GRID_SIZE) + 1,
+        x: Math.floor(Math.random() * GRID_SIZE) + 1,
         y: Math.floor(Math.random() * GRID_SIZE) + 1
     }
 }
@@ -14,3 +14,4 @@ export function outsideGrid(position) {
         position.y < 1 || position.y > GRID_SIZE
     )
 }
+
